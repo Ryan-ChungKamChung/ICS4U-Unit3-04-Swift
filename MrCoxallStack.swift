d64d62a54810370e809b9b515710cd01c32d9b3b
Ubuntu <ubuntu@ip-172-31-92-172.ec2.internal>
2021-12-11 18:15:32 +0000

    added *.swift

---
+++ MrCoxallStack.swift
@@ -0,0 +1,55 @@
+//
+//  MrCoxallStack.swift
+//
+//  Created by Ryan Chung
+//  Created on 2021-12-09
+//  Version 1.0
+//  Copyright (c) 2021 Ryan Chung. All rights reserved.
+//
+//  This is the stack class.
+//
+
+// Class that emulates a stack
+class MrCoxallStack<T> {
+
+    // Custom error types
+    enum Errors: Error {
+        case emptyStackException
+    }
+
+    // ArrayList signifying a stack
+    private var stackAsArray = [T]()
+
+    // Pushes a number to the stack
+    func push(pushElement: T) {
+        self.stackAsArray.insert(pushElement, at: 0)
+    }
+
+    // Pops the first element in the stack
+    func pull() throws -> T {
+        if self.stackAsArray.count == 0 {
+            throw Errors.emptyStackException
+        } else {
+            return self.stackAsArray.removeFirst()
+        }
+    }
+
+    // Returns the first element in the stack
+    func peek() throws -> T {
+        if self.stackAsArray.count == 0 {
+            throw Errors.emptyStackException
+        } else {
+            return self.stackAsArray[0]
+        }
+    }
+
+    // Clears the stack
+    func clear() {
+        self.stackAsArray.removeAll()
+    }
+
+    // Displays the stack
+    func showStack() {
+        self.stackAsArray.forEach { element in print("\(element)")}
+    }
+}
---
+++ main.swift
@@ -0,0 +1,57 @@
+//
+//  main.swift
+//
+//  Created by Ryan Chung
+//  Created on 2021-12-09
+//  Version 1.0
+//  Copyright (c) 2021 Ryan Chung. All rights reserved.
+//
+//  This program demonstrates a stack.
+//
+
+import Foundation
+
+// Takes user input in a loop and permits the user to make multiple commands
+// exit = end of program
+let aStack = MrCoxallStack<Int>()
+
+let allowedCommands = ["push", "pull", "show", "peek", "clear", "exit"]
+print("Acceptable Commands:\npush <number> - pull - show - peek - clear - exit")
+
+while true {
+    print("> ", terminator: "")
+    let userInput = readLine()
+
+    if userInput != nil {
+        if userInput!.starts(with: allowedCommands[0]) {
+            let userCommandArray = userInput!.components(separatedBy: " ")
+            if let userNumber = Int(userCommandArray[1]) {
+                aStack.push(pushElement: userNumber)
+            } else {
+                print("Please enter a valid command.")
+            }
+        } else if userInput!.starts(with: allowedCommands[1]) {
+            do {
+                try print("Removed: \(aStack.pull())")
+            } catch {
+                print("The stack is empty.")
+            }
+        } else if userInput!.starts(with: allowedCommands[2]) {
+            aStack.showStack()
+        } else if userInput!.starts(with: allowedCommands[3]) {
+            do {
+                try print("\(aStack.peek())")
+            } catch {
+                print("The stack is empty.")
+            }
+        } else if userInput!.starts(with: allowedCommands[4]) {
+            aStack.clear()
+        } else if userInput!.starts(with: allowedCommands[5]) {
+            break
+        } else {
+            print("Please enter a valid command.")
+        }
+    }
+}
+
+print("\nDone.")
